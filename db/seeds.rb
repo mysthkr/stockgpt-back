6c6de102d959066ca86ded8eabd1572e45fbc061
mysthkr4036@gmail.com <hikaru_miyashita>
2023-05-09 13:43:57 +0900

    Edit seed for devise

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,11 +14,11 @@ users = [
   ["testform@gmail.com", 1, "testform", true, true], 
   ["testform2@gmail.com", 2, "testform2", false, true], 
   ["testform3@gmail.com", 3, "testform3", false, false],
-  ["admin@gmail.com", 1, "admin", true, false], 
+  ["admin@gmail.com", 1, "adminpass", true, false], 
 ]
 
-users.each do |email, group_id, encrypted_password, admin, business| 
-  User.create(email: email, group_id: group_id, encrypted_password: encrypted_password, 
+users.each do |email, group_id, password, admin, business| 
+  User.create(email: email, group_id: group_id, password: password, 
   admin: admin, business: business)
 end
 
